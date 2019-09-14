dd63715729538f9efcc1741a5737d8ba4ee842a7
Albrecht Márk <alb.mark94@gmail.com>
2019-09-14 21:08:30 +0200

    Created

---
+++ nutrition-diary/src/main/java/com/example/test/model/MaterialNutrient.java
@@ -0,0 +1,71 @@
+package com.example.test.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MaterialNutrients")
+public class MaterialNutrient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	private Material material;
+	
+	@ManyToOne
+	private Nutrient nutrient;
+	
+	private int quantity;
+
+	
+	
+	
+	public MaterialNutrient() {
+		super();
+	}
+
+	public MaterialNutrient(int quantity) {
+		super();
+		this.quantity = quantity;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Material getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(Material material) {
+		this.material = material;
+	}
+
+	public Nutrient getNutrient() {
+		return nutrient;
+	}
+
+	public void setNutrient(Nutrient nutrient) {
+		this.nutrient = nutrient;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
