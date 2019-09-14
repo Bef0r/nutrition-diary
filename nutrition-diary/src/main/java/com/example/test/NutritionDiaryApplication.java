c742cf541e68be8dba0b5bdd0f9ce7954a9a40d4
Albrecht Márk <alb.mark94@gmail.com>
2019-09-14 11:34:33 +0200

    Created projekt

---
+++ nutrition-diary/src/main/java/com/example/test/NutritionDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionDiaryApplication.class, args);
+	}
+
+}
