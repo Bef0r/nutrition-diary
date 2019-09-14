08b391628dac120fc69833d8af50636e3a224ec0
Albrecht Márk <alb.mark94@gmail.com>
2019-09-14 12:01:02 +0200

    Created base dose class

---
+++ nutrition-diary/src/main/java/com/example/test/model/Dose.java
@@ -0,0 +1,46 @@
+package com.example.test.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+public class Dose {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NaturalId
+	private String name;
+
+	public Dose() {}
+	
+	public Dose(String name) {
+		super();
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
