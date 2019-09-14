3c23584847a67f9f895e0e4932a504b8032aedbb
Albrecht Márk <alb.mark94@gmail.com>
2019-09-14 19:45:47 +0200

    Created class

---
+++ nutrition-diary/src/main/java/com/example/test/model/Unit.java
@@ -0,0 +1,52 @@
+package com.example.test.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name="Units")
+public class Unit {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NaturalId
+	private String name;
+	
+	
+	
+	public Unit() {
+		super();
+	}
+	
+	public Unit(String name) {
+		super();
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
